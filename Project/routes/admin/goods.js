1decf8fec187d92a70c3492a97841f00de41108d
阮彥誌 <billy851120@gmail.com>
2022-06-24 23:42:09 +0800

    6/24

--- Project/routes/admin/goods.js
+++ Project/routes/admin/goods.js
@@ -1,8 +1,12 @@
+const { json } = require('body-parser');
 var express = require('express');
 var router = express.Router();
+var db = require('../../dataBase');
 
 router.get('/', function (rqs, res) {
-  res.render('admin_index', { title: '後台管理系統' });
+  db.exec('SELECT * FROM customer_id', [], (result, fields) => {
+    res.render('admin_index', { data: result });
+  });
 });
 router.get('/11', function (rqs, res) {
   res.render('admin_11', { title: '後台管理系統' });
