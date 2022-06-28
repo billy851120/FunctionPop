221dd454ec81cc8b6669375003dfee1bd8913061
阮彥誌 <billy851120@gmail.com>
2022-06-28 16:17:09 +0800

    6/28

--- Project/routes/home/product.js
+++ Project/routes/home/product.js
@@ -8,8 +8,16 @@ var bodyParser = require('body-parser');
 
 
 
-router.use(session({secret:"secret"}));
+router.use(session({
+  secret:"secret",
+  name:"product"
+}));
 router.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
 
 
 router.get('/', function (rqs, res) {
@@ -36,77 +44,8 @@ router.get('/single_product/:id', function (rqs, res) {
 
 
 
-function isProductInCart(cart,id) {
-  for (let i = 0; i < cart.length; i++) {
-    if(cart[i].id == id){
-        return true;
-    }
-  }
-
-  return false;
-}
 
 
-function calculateTotal(cart,req) {
-    total = 0;
-    for(let i = 0 ; i <cart.length; i++){
-        // if we are offering a discounted price 
-        if(cart[i].price){
-            total = total + (cart[i].price*cart[i].quantity);
-        }else{
-            total = total + (cart[i].price*cart[i].quantity);
-        }
-    }
-    req.session.total = total;
-    return total;
-}
-
-
-
-
-router.post('/add_to_cart', function(req,res){
-
-    var code = req.body.code;
-    var name = req.body.name;
-    var image = req.body.image;
-    var price = req.body.price;
-    var quantity = req.body.quantity;
-    var product = {code:code, name:name, image:image, price:price, quantity:quantity};
-
-
-    if(req.session.cart){
-        var cart = req.session.cart;
-
-        if(!isProductInCart(cart,code)){
-            // JSON.parse(cart).push(product);
-            console.log(cart) ;
-            cart.push(product);
-        }
-    }else{
-
-        req.session.cart = [product];
-        var cart = req.session.cart;
-    }
-
-
-    // Calculate total
-    calculateTotal(cart,req);
-
-    // return to cart page
-    res.redirect('/home/product/add_to_cart');
-
-});
-
-router.get('/add_to_cart',function (req,res) {
-    
-    var cart = req.session.cart;
-    var total = req.session.total;
-    console.log(cart,total);
-
-
-    res.render('shop_cart', {cart:cart, total:total});
-
-});
 
 
 
--- Project/routes/home/shop_cart.js
+++ Project/routes/home/shop_cart.js
@@ -5,53 +5,85 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
+
+function isProductInCart(cart,id) {
+  for (let i = 0; i < cart.length; i++) {
+    if(cart[i].id == id){
+        return true;
+    }
+  }
+
+  return false;
+}
+
+
+function calculateTotal(cart,req) {
+    total = 0;
+    for(let i = 0 ; i <cart.length; i++){
+        // if we are offering a discounted price 
+        if(cart[i].price){
+            total = total + (cart[i].price*cart[i].quantity);
+        }else{
+            total = total + (cart[i].price*cart[i].quantity);
+        }
+    }
+    req.session.total = total;
+    return total;
+}
+
 router.use(session({secret:"secret"}));
 router.use(bodyParser.urlencoded({extended:true}));
 
+router.post('/', function(req,res){
 
-// app.post('/cart', (req, res) => {
-//     // fs.readFile(CART_DATA_FILE, (err, data) => {
-//     //   const cartProducts = JSON.parse(data);
-//     //   const newCartProduct = {
-//     //     id: req.body.id,
-//     //     title: req.body.title,
-//     //     description: req.body.description,
-//     //     price: req.body.price,
-//     //     image_tag: req.body.image_tag,
-//     //     quantity: 1
-//     //   };
-//     //   let cartProductExists = false;
-//     //   cartProducts.map((cartProduct) => {
-//     //     if (cartProduct.id === newCartProduct.id) {
-//     //       cartProduct.quantity++;
-//     //       cartProductExists = true;
-//     //     }
-//     //   });
-//     //   if (!cartProductExists) cartProducts.push(newCartProduct);
-//     //   fs.writeFile(CART_DATA_FILE, JSON.stringify(cartProducts, null, 4), () => {
-//     //     res.setHeader('Cache-Control', 'no-cache');
-//     //     res.json(cartProducts);
-//     //   });
-//     // });
+    var code = req.body.code;
+    var name = req.body.name;
+    var image = req.body.image;
+    var price = req.body.price;
+    var quantity = req.body.quantity;
+    var product = {code:code, name:name, image:image, price:price, quantity:quantity};
 
-//     console.log('123');
-//   });
 
-  
+    if(req.session.cart){
+        var cart = req.session.cart;
 
-// router.get('/', function (rqs, res) {
-//   var cart = rqs.session.cart;
-//     var total = rqs.session.total;
-//     console.log(cart,total)
+        if(!isProductInCart(cart,code)){
+            console.log(cart) ;
+            cart.push(product);
+        }
+    }else{
 
-//     res.render('shop_cart', {cart:cart, total:total});
-// });
+        req.session.cart = [product];
+        var cart = req.session.cart;
+    }
+
+
+    // Calculate total
+    calculateTotal(cart,req);
+
+    // return to cart page
+    res.redirect('back');
+    // res.send('adasda');
+    // res.end()
+});
+
+router.get('/',function (req,res) {
+    
+    var cart = req.session.cart;
+    var total = req.session.total;
+    console.log(cart,total);
+    console.log(req.session);
+
+
+    res.render('shop_cart', {cart:cart, total:total});
+
+});
 router.get('/orderCheck', function (rqs, res) {
   res.render('orderCheck', { title: '訂單確認' });
 });
-router.get('/lab',function(rqs,res){
-  res.render('lab',{title:"sss"})
-})
+// router.get('/lab',function(rqs,res){
+//   res.render('lab',{title:"sss"})
+// })
 
 module.exports = router;
 
