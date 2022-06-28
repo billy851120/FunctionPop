ccdae3e92a911c06434e92105d427d9648fd05d9
Aliee <luoyanrong11@gmail.com>
2022-06-28 14:34:55 +0800

    06/28

--- Project/routes/home.js
+++ Project/routes/home.js
@@ -6,11 +6,13 @@ var customRouter = require('./home/custom');
 var shopCartRouter = require('./home/shop_cart');
 var memberRouter = require('./home/member');
 var productRouter = require('./home/product');
+var registerRouters = require('./home/register');
 
 router.use('/', indexRouter);
 router.use('/custom', customRouter);
 router.use('/shop_cart', shopCartRouter);
 router.use('/member', memberRouter);
 router.use('/product', productRouter);
+router.use('/register', registerRouters);
 
 module.exports = router;
